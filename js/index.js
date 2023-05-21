696f5d01f92dfba1e5a5200a44e40cf4ab99cf89
Ryan Gan <49723210+ryanzg123@users.noreply.github.com>
2023-05-21 18:58:17 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,126 @@
+/**
+ * 解析歌词字符串
+ * 得到一个歌词对象的数组
+ * 每个歌词对象
+ * { time: 开始时间， words: 歌词内容 }
+ */
+
+
+function parseLrc() {
+    let lines = lrc.split('\n');
+    let results = []; // lyric list
+    for (let i = 0; i < lines.length; i++) {
+        let str = lines[i];
+        let parts = str.split(']');
+        let timeStr = parts[0].substring(1);
+
+        let obj = {
+            time: parseTime(timeStr),
+            lyrics: parts[1]
+        };
+
+        results.push(obj);
+    }
+    return results;
+}
+
+/**
+ * 将一个时间字符串解析为数字（秒）
+ * @param {String} timeStr 时间字符串
+ * @returns 
+ */
+
+function parseTime(timeStr) {
+    let parts = timeStr.split(':');
+    return +parts[0] * 60 + +parts[1];
+}
+
+let lyricData = parseLrc();
+
+// get the required dom elements
+let doms = {
+    audio: document.querySelector('audio'),
+    ul: document.querySelector('.container ul'),
+    container: document.querySelector('.container'),
+}
+
+/**
+ * Calculate at the current time of the audio player
+ * which lyric should be displayed from the lyricData
+ * if no lyric needs to be displayed, get -1
+ */
+function findIndex() {
+    let curTime = doms.audio.currentTime;
+    for (let i = 0; i < lyricData.length; i++) {
+        if (curTime < lyricData[i].time) {
+            return i - 1;
+        }
+    }
+
+    // can't find, return the last one
+    return lyricData.length - 1;
+}
+
+// UI
+
+/**
+ * Create a lyric element li
+ */
+// function createLyricElement() {
+//     for(let i = 0; i < lyricData.length; i++) {
+//         let li = document.createElement('li');
+//         li.textContent = lyricData[i].lyrics;
+//         doms.ul.appendChild(li); // modified dom tree
+//     }
+// }
+
+/**
+ * Optimized version of createLyricElement
+ * Create a fragment and append it to the dom tree
+ */
+ function createLyricElement() {
+    let fragment = document.createDocumentFragment(); // document fragment
+    for(let i = 0; i < lyricData.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = lyricData[i].lyrics;
+        fragment.appendChild(li); // modified fragment
+    }
+    doms.ul.appendChild(fragment); // modified dom tree
+ }
+
+ createLyricElement();
+
+ const containerHeight = doms.container.clientHeight; // container height
+ const liHeight = doms.ul.children[0].clientHeight; // every li height
+ const maxOffset = doms.ul.clientHeight - containerHeight; // max offset
+
+ /**
+  * set ul offset when time changes
+  */
+ function setOffset() {
+    let index = findIndex();
+    let offset = liHeight * index + liHeight / 2 - containerHeight / 2;
+
+    if (offset < 0) {
+        offset = 0;
+    }
+
+    if (offset > maxOffset) {
+        offset = maxOffset;
+    }
+
+    doms.ul.style.transform = `translateY(-${offset}px)`;
+
+    // remove active class
+    let li = doms.ul.querySelector('.active');
+    if (li) {
+        li.classList.remove('active');
+    }
+
+    li = doms.ul.children[index];
+    if (li) {
+        li.classList.add('active');
+    }
+ }
+
+ doms.audio.addEventListener('timeupdate', setOffset);
---
+++ lyrics/50feet.js
@@ -0,0 +1,73 @@
+const lrc1 = `[00:00.00]50 Feet By SoMo
+[00:00.15]50 50 feet up underground
+[00:04.21]You know I keep my feet on the sound
+[00:07.93]50 50 feet up underground
+[00:12.00]You know I keep my feet on the sound
+[00:15.51]I'm around baby I'm around
+[00:19.23]Underground but you know I'm around
+[00:23.07]I was down, lately I've been down
+[00:26.77]Ran around now I'm back I'm found
+[00:30.57]Pour the wine up
+[00:32.36]You're silent
+[00:34.51]Why though?
+[00:36.74]You know you like this style of this side
+[00:42.28]I know you like it
+[00:44.74]I fucked you right
+[00:46.14]I did
+[00:46.86]I loved you every single night
+[00:48.51]You know you like that, dear
+[00:50.37]You know you like it when I ride
+[00:52.41]You owe me all of it
+[00:54.13]You told every single time
+[00:56.18]I am holding on to it
+[00:58.13]I am holding everything that's mine
+[01:00.15]I fucked you right
+[01:01.61]I did
+[01:02.15]I loved you every single night
+[01:03.76]You know you like that, dear
+[01:05.65]You know you like it when I ride
+[01:07.81]You owe me all of it
+[01:09.73]You told me every single time
+[01:11.45]I'm holding on to it
+[01:13.63]I'm holding everything that's mine
+[01:15.15]You know it
+[01:16.53]Popped a pill whats the deal
+[01:18.36]I don't feel it anymore
+[01:20.09]Popped, popped a pill
+[01:21.20]What's that? you know
+[01:22.30]I've down on the floor
+[01:24.04]I cried everyday you know
+[01:25.91]This ain't real anymore
+[01:27.92]It's a dream on the door
+[01:29.66]It's a dream on the board
+[01:31.71]It's the life and you warned me
+[01:33.69]Hold tighter
+[01:35.38]Reminiscing on old times
+[01:37.12]With my red lighter
+[01:39.13]Said you feel it in your throat when I sing
+[01:42.51]You know what I mean
+[01:44.37]You know what I mean
+[01:45.91]I fucked you right
+[01:47.00]I did
+[01:47.60]I loved you every single night
+[01:49.52]You know you like that, dear
+[01:51.34]You know you like it when I ride
+[01:53.23]You owe me all of it
+[01:55.35]You told every single time
+[01:57.08]I am holding on to it
+[01:59.07]I am holding everything that's mine
+[02:01.05]I fucked you right
+[02:02.55]I did
+[02:02.90]I loved you every single night
+[02:04.89]You know you like that, dear
+[02:06.66]You know you like it when I ride
+[02:08.47]You owe me all of it
+[02:10.34]You told me every single time
+[02:12.38]I'm holding on to it
+[02:14.55]I'm holding everything that's mine
+[02:16.17]You know it
+[02:17.92]50 50 feet up underground
+[02:21.65]You know I keep my feet on the sound
+[02:25.15]50 50 feet up underground
+[02:29.33]You know I keep my feet on the sound
+[02:32.13]`
